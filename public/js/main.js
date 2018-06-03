036577bf0420dce887f90a2b78452040913bad80
Enakelemichael <enakelemichael@yahoo.com>
2018-06-03 18:00:57 +0200

    done

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,13 @@
-$('#my_progress').appear();
-
-function my_progress(barID, value) {
-$(barID).delay(500).animate({'width': value
-}, 500);
+function move() {
+  var elem = document.getElementBy("progress1");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
 }
-$('#my_progress').on('appear', function () {
-my_progress('#bar1', '80%');
-my_progress('#bar2', '70%');
-my_progress('#bar3', '70%');
-my_progress('#bar4', '50%');
-my_progress('#bar5', '70%');
-my_progress('#bar6', '70%');
-my_progress('#bar7', '60%');
-my_progress('#bar8', '50%');
-});

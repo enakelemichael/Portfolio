b38e6695a967a3732e07bf70cee2270f363137e8
Enakelemichael <enakelemichael@yahoo.com>
2018-06-03 17:46:24 +0200

    git commit -m "done"

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,16 @@
-$('#bar').appear();
- 
-function bar(progressID, value) {
-    $(progressID).delay(500).animate({
-        'width': value
-    }, 500);
+$('#my_progress').appear();
+
+function my_progress(barID, value) {
+$(barID).delay(500).animate({'width': value
+}, 500);
 }
-$('#bar').on('appear', function () {
-  bar('#progress', '80%');
-    my_progress('#bar2', '70%');
-    my_progress('#bar3', '70%');
-    my_progress('#bar4', '50%');
-    my_progress('#bar5', '70%');
-    my_progress('#bar6', '70%');
-    my_progress('#bar7', '60%');
-    my_progress('#bar8', '50%');
+$('#my_progress').on('appear', function () {
+my_progress('#bar1', '80%');
+my_progress('#bar2', '70%');
+my_progress('#bar3', '70%');
+my_progress('#bar4', '50%');
+my_progress('#bar5', '70%');
+my_progress('#bar6', '70%');
+my_progress('#bar7', '60%');
+my_progress('#bar8', '50%');
 });
